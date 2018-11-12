9475f77e0155be77369dc0908114eef9d87804fb
Regis.pkp <regis@pinkpepper-studio.com>
2018-11-12 14:15:30 +0100

    gestion du multifile: on peut utilier l'outil pour plusieurs agenda

--- tools/agenda.php
+++ tools/agenda.php
@@ -1,75 +1,78 @@
 <?php
 
-if (!empty($_FILES) && !empty($_POST)){
+if (!empty($_FILES) && !empty($_POST)) {
     include('SG_iCal.php');
     $dateFilterStart = $_POST["dateStart"];
     $dateFilterEnd = $_POST["dateEnd"];
     $timeStampStartFilter = strtotime($dateFilterStart . "00:00:00");
     $timeStampEndFilter = strtotime($dateFilterEnd . "23:59:59");
 
-    $calebdarInfo = $_FILES['inputFile'];
-    $urlCalendar = $_FILES['inputFile']['tmp_name'];
     $array = array();
-
-    $ical = new SG_iCalReader($urlCalendar);
-
-
-    foreach ($ical->getEvents() As $event) {
-
-        $summary = $event->getSummary();
-        $timeStart = $event->getStart();
-        $timeEnd = $event->getEnd();
-
-        $dayStart = date("d-m-Y", $timeStart);
-
-        $timeTacheTstamp = $timeEnd - $timeStart;
-        if ($timeStart >= $timeStampStartFilter && $timeEnd <= $timeStampEndFilter){
-
-            // Séparer summary pour obtenir le client + la tache.
-            // Detecter si - présent dans summary
-            if (strpos($summary, ' - ')) {
-                // Si oui alors explode
-                list($client, $tache) = explode(" - ", $summary, 2);
-            } else {
-                // Si non alors tache autre
-                $client = "Autre";
-                $tache = $summary;
-            }
-
-
-            // Detection si clé déjà présente dans tableau
-            if (isset($array[$client][$tache])) {
-                // Si oui valeur + 1
-                // Obtenir un tableau de forme $array[$client][$tache] = $nbFois
-                $array[$client][$tache]['heures'] += $timeTacheTstamp;
-
-                if (strpos($array[$client][$tache]['jours'] , $dayStart) === false) {
-                    $array[$client][$tache]['jours'] .= ", " . $dayStart;
-                }
-            } else {
-                // Si non valeur à 1
-                $array[$client][$tache]['heures'] = $timeTacheTstamp;
-                $array[$client][$tache]['jours'] = $dayStart;
-
-            }
-        }
-    }
-
     $total = 0;
-    foreach ($array as $client => $taches){
-        $array[$client]["Total"]['heures'] = 0 ;
-        $array[$client]["Total"]['jours'] = "" ;
-        foreach ($taches as $tache => $timeTacheTstamp){
-            $total += $timeTacheTstamp['heures']/3600;
-            $array[$client][$tache]['heures'] = $timeTacheTstamp['heures']/3600;
-            $array[$client]["Total"]['heures'] += $timeTacheTstamp['heures']/3600 ;
+
+    foreach ($_FILES['inputFile']['tmp_name'] as $fichiersTmp) {
+        $urlCalendar = $fichiersTmp;
+
+        $ical = new SG_iCalReader($urlCalendar);
+
+
+        foreach ($ical->getEvents() As $event) {
+
+            $summary = $event->getSummary();
+            $timeStart = $event->getStart();
+            $timeEnd = $event->getEnd();
+
+            $dayStart = date("d-m-Y", $timeStart);
+
+            $timeTacheTstamp = $timeEnd - $timeStart;
+            if ($timeStart >= $timeStampStartFilter && $timeEnd <= $timeStampEndFilter) {
+
+                // Séparer summary pour obtenir le client + la tache.
+                // Detecter si - présent dans summary
+                if (strpos($summary, ' - ')) {
+                    // Si oui alors explode
+                    list($client, $tache) = explode(" - ", $summary, 2);
+                } else {
+                    // Si non alors tache autre
+                    $client = "Autre";
+                    $tache = $summary;
+                }
+
+
+                // Detection si clé déjà présente dans tableau
+                if (isset($array[$client][$tache])) {
+                    // Si oui valeur + 1
+                    // Obtenir un tableau de forme $array[$client][$tache] = $nbFois
+                    $array[$client][$tache]['heures'] += $timeTacheTstamp;
+
+                    if (strpos($array[$client][$tache]['jours'], $dayStart) === false) {
+                        $array[$client][$tache]['jours'] .= ", " . $dayStart;
+                    }
+                } else {
+                    // Si non valeur à 1
+                    $array[$client][$tache]['heures'] = $timeTacheTstamp;
+                    $array[$client][$tache]['jours'] = $dayStart;
+
+                }
+            }
+        }
+
+    }
+    foreach ($array as $client => $taches) {
+        $array[$client]["Total"]['heures'] = 0;
+        $array[$client]["Total"]['jours'] = "";
+        foreach ($taches as $tache => $timeTacheTstamp) {
+            $total += $timeTacheTstamp['heures'] / 3600;
+            $array[$client][$tache]['heures'] = $timeTacheTstamp['heures'] / 3600;
+            $array[$client]["Total"]['heures'] += $timeTacheTstamp['heures'] / 3600;
         }
     }
 
-    if ($_POST["choix"] === "csv"){
+
+    if ($_POST["choix"] === "csv") {
         header("Content-Type: text/csv");
-        header("Content-Disposition: attachment; filename=temps " .$dateFilterStart. "_" . $dateFilterEnd .".csv");
-        echo"\xEF\xBB\xBF";
+        header("Content-Disposition: attachment; filename=temps " . $dateFilterStart . "_" . $dateFilterEnd . ".csv");
+        echo "\xEF\xBB\xBF";
 
         $fp = fopen('PHP://output', 'w');
         fputs($fp, $bom = (chr(0xEF) . chr(0xBB) . chr(0xBF)));
@@ -86,10 +89,10 @@ if (!empty($_FILES) && !empty($_POST)){
         fclose($fp);
         die();
 
-    } elseif ($_POST["choix"] === "affichage"){
+    } elseif ($_POST["choix"] === "affichage") {
         ?>
         <head>
-            <meta charset="utf-8" />
+            <meta charset="utf-8"/>
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <title>PkpAgenda</title>
             <link rel="stylesheet" type="text/css" href="style.css">
@@ -108,14 +111,17 @@ if (!empty($_FILES) && !empty($_POST)){
             <tbody>
             <?php
 
-            foreach ($array as $client => $taches){
-                foreach ($taches as $tache => $timeTacheTstamp){
-                    ?><tr>
+            foreach ($array as $client => $taches) {
+                foreach ($taches as $tache => $timeTacheTstamp) {
+                    ?>
+                    <tr>
                     <td><?php echo $client ?></td>
-                    <td><?php echo $tache?></td>
+                    <td><?php echo $tache ?></td>
                     <td><?php echo $timeTacheTstamp['heures'] ?></td>
-                    <td><?php echo number_format($timeTacheTstamp['heures'] / 7 , 2)?></td>
-                    <td><?php if ($tache !== 'Total'){echo $array[$client][$tache]['jours']; }?></td>
+                    <td><?php echo number_format($timeTacheTstamp['heures'] / 7, 2) ?></td>
+                    <td><?php if ($tache !== 'Total') {
+                            echo $array[$client][$tache]['jours'];
+                        } ?></td>
                     </tr><?php
                 }
             }
@@ -131,7 +137,7 @@ if (!empty($_FILES) && !empty($_POST)){
                 </td>
                 <td>
                     <?php
-                    echo number_format($total / 7, 2) ;
+                    echo number_format($total / 7, 2);
                     ?>
                 </td>
             </tr>
@@ -143,20 +149,20 @@ if (!empty($_FILES) && !empty($_POST)){
 ?>
 
 <?php
-    if(empty($_POST['choix']) || $_POST['choix'] != 'affichage') {
+if (empty($_POST['choix']) || $_POST['choix'] != 'affichage') {
     ?>
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>PkpAgenda</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <?php
-    }
+}
 ?>
 
 <form method="post" enctype="multipart/form-data">
-    <input type="file" id="inputFile" name="inputFile"/>
+    <input type="file" id="inputFile" name="inputFile[]" multiple/>
     <br/>
     <label for=dateStart">date de début</label>
     <input type="date" id="dateStart" name="dateStart"/>
